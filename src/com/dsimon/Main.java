c1a612467012978c9edc5b59f98365fcec38ca93
Hurlyburly2 <dougdougmann@gmail.com>
2019-06-20 00:22:05 -0400

    challenge complete

--- src/com/dsimon/Main.java
+++ src/com/dsimon/Main.java
@@ -7,6 +7,28 @@ public class Main {
         System.out.println("New score is " + newScore);
         calculateScore(75);
         calculateScore();
+
+        System.out.println(calcFeetAndInchesToCentimeters(5, 2));
+        System.out.println(calcFeetAndInchesToCentimeters(6));
+        System.out.println(calcFeetAndInchesToCentimeters(16));
+
+        System.out.println(calcFeetAndInchesToCentimeters(100));
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+        if (feet < 0 || inches < 0 || inches > 12) {
+            return -1;
+        }
+        inches = inches + (feet * 12);
+        return inches * 2.54;
+
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double inches) {
+        if (inches < 0) {
+            return -1;
+        }
+        return calcFeetAndInchesToCentimeters((int) inches/12, (int) inches % 12);
     }
 
     public static int calculateScore(String playerName, int score) {
